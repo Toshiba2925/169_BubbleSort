e89c327af33f528ea908d0420e7f54ea54540d98
Toshiba2925 <aqifashiba25@gmail.com>
2025-03-13 11:33:43 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int arr[20];  //Deklarasi variabel global array dengan panjang 20
+int n;  //Deklarasi variabel global n untuk menyimpan elemen pada array
+
+int main(){
+
+}
+    
